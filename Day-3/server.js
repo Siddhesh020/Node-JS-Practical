454b9fb250e5555d3ab6d719b87780103bf05c70
Siddhesh020 <siddheshd776@gmail.com>
2026-01-29 23:54:39 +0530

    Node JS: Day 1, 2, 3

---
+++ Day-3/server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, resp) => {
+    let path = "./views/";
+    switch (req.url) {
+        case "/":
+            path = path + "home.html";
+            break;
+        case "/about":
+            path = path + "about.html";
+            break;
+        case "/contact":
+            path = path + "contact.html";
+            break; 
+        default:
+            path = path + "error.html";
+            break;
+    }
+
+    fs.readFile(path, (err,data) => {
+        if(err){
+            console.log(err);
+        } else{
+            resp.end(data);
+        }
+    })
+})
+
+server.listen(3000, () => {
+    console.log("Server listening on port 3000");
+})
